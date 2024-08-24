0c54c6c60967ce643a00e5861e0044765786f353
stilt0n <mattias.leino@arts.cornish.edu>
2024-08-24 18:19:02 -0400

    add and configure prettier

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+import { Config } from 'prettier';
+
+const config: Config = {
+	trailingComma: 'es5',
+	tabWidth: 2,
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+};
+
+export default config;
