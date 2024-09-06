c8e47c081b0c48f958d21f081f780dc1b727f2fd
stilt0n <mattias.leino@arts.cornish.edu>
2024-09-06 02:20:41 -0400

    connect editor input to hidden input

--- components/recipe-edit-form.client.tsx
+++ components/recipe-edit-form.client.tsx
@@ -1,38 +1,69 @@
 'use client';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Input } from '@/components/ui/input';
 import { FormInput } from '@/components/form/form-input';
 import { Button } from '@/components/ui/button';
 import EditorInput from '@/components/editor';
-import { useState } from 'react';
+import { useEffect } from 'react';
 
 export interface RecipeFormProps {
   recipeData?: RecipeData;
 }
 
 export const RecipeForm = (props: RecipeFormProps) => {
-  // this is not the way to do it with react-hook-form
-  const [editorState, setEditorState] = useState('');
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors: e },
+  } = useForm({
+    resolver: zodResolver(recipeSchema),
+  });
+  const onSubmit = handleSubmit((data) => {
+    console.log('anything');
+    console.log(data);
+  });
+
+  useEffect(() => {
+    const subscription = watch((value, { name }) => {
+      if (name === 'instructions') {
+        console.log(value.instructions);
+      }
+    });
+    return () => subscription.unsubscribe();
+  });
+
   return (
-    <form className='h-full flex flex-col gap-y-4 p-4 lg:p-8'>
-      <FormInput label='Recipe name' type='text' required />
-      <FormInput label='Author' type='text' />
+    <form
+      className='h-full flex flex-col gap-y-4 p-4 lg:p-8'
+      onSubmit={onSubmit}
+    >
+      <FormInput
+        label='Recipe name'
+        type='text'
+        required
+        {...register('name')}
+      />
+      <FormInput label='Author' type='text' {...register('author')} />
       <FormInput
         label='Recipe time'
         type='text'
         placeholder='e.g. 30 minutes'
+        {...register('recipeTime')}
       />
-      <FormInput label='Image url' type='text' />
+      <FormInput label='Image url' type='text' {...register('imageUrl')} />
       <div>
-        <Input type='hidden' value={editorState} />
+        <Input type='hidden' {...register('instructions')} />
         <EditorInput
-          onChange={(editorContent) => setEditorState(editorContent)}
+          onChange={(editorContent) => setValue('instructions', editorContent)}
           menuAriaLabel='instructions editor menu'
           label='Recipe instructions'
         />
       </div>
-      <Button>Submit</Button>
+      <Button type='submit'>Submit</Button>
     </form>
   );
 };
