b029c8874955a655bf367a25959aec84450b594f
stilt0n <mattias.leino@arts.cornish.edu>
2024-09-07 16:10:56 -0400

    prototype insert with mock db

--- app/actions/form-submit.ts
+++ app/actions/form-submit.ts
@@ -2,7 +2,15 @@
 
 import type { RecipeData } from '@/types/recipeTypes';
 
+const mockDb: RecipeData[] = [];
+
 export const formSubmitAction = (data: RecipeData) => {
   // for now I just want to get this data visible on the server
+  console.log('inserting data:');
   console.log(data);
+  mockDb.push(data);
+  console.log(`recipe count: ${mockDb.length}`);
+  console.log(
+    `recipe titles:\n${mockDb.map((data) => data.title).join('\n')}\n`
+  );
 };
