ed8d609948288aef101eaf591b5fe3d561c84021
stilt0n <mattias.leino@arts.cornish.edu>
2024-09-07 15:13:03 -0400

    log form data on server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 'use client';
 
 import { RecipeForm } from '@/components/recipe-edit-form.client';
+import { useTransition } from 'react';
+import { formSubmitAction } from '@/app/actions/form-submit';
 
 export default function Home() {
+  const [, startTransition] = useTransition();
   return (
     <div>
       <h1 className='text-2xl text-center mt-2'>Recipe Form</h1>
       <RecipeForm
-        onSubmitSuccess={(data, event) => {
-          event?.preventDefault();
+        onSubmitSuccess={(data) => {
           console.log(data);
+          startTransition(() => {
+            formSubmitAction(data);
+          });
         }}
         onSubmitError={(e) => {
           console.log(e);
