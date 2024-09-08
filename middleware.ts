b7606af7cf5f1f75d62f79f4eff8fbf9929ed3ac
stilt0n <mattias.leino@arts.cornish.edu>
2024-09-08 13:39:16 -0400

    setup clerk

---
+++ middleware.ts
@@ -0,0 +1,38 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isWritePermissionRoute = createRouteMatcher([
+  '/create-recipe(.*)',
+  '/edit-recipe(.*)',
+]);
+
+const isAdminPermissionRoute = createRouteMatcher(['/(api|trpc)(.*)']);
+
+export default clerkMiddleware((auth, req) => {
+  if (isWritePermissionRoute(req)) {
+    console.log('in write permission route');
+    auth().protect(() => {
+      const { userId, sessionClaims } = auth();
+      return (
+        !!userId &&
+        (sessionClaims?.metadata?.role === 'family' ||
+          sessionClaims?.metadata?.role === 'admin')
+      );
+    });
+  }
+
+  if (isAdminPermissionRoute(req)) {
+    auth().protect(() => {
+      const { userId, sessionClaims } = auth();
+      return !!userId && sessionClaims?.metadata?.role === 'admin';
+    });
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
