a83d0d0298bc5b4b68cf08077bad1d9fc2d347ff
stilt0n <mattias.leino@arts.cornish.edu>
2024-09-10 21:44:04 -0400

    commit initial loadRecipes mock

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const images = {
+  grits:
+    'https://images.unsplash.com/photo-1559852925-6a5a7125525b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  greenbeans:
+    'https://images.unsplash.com/photo-1448293065296-c7e2e5b76ae9?q=80&w=2014&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+};
