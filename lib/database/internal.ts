2238a27999af69f50166915e08a73d49bf139668
stilt0n <mattias.leino@arts.cornish.edu>
2024-09-12 01:06:50 -0400

    add pagination to database api

--- lib/database/internal.ts
+++ lib/database/internal.ts
@@ -1,10 +1,51 @@
+import type { SQLiteSelect, SelectedFields } from 'drizzle-orm/sqlite-core';
+import { count } from 'drizzle-orm';
 import { db } from '@/db';
 import { recipes, InsertRecipe } from '@/db/schema';
 
-// It will probably make sense to give all get methods arguments to support
-// lazy loading if there could plausibly be a large number of values returned
-export const getRecipes = async () => {
-  return db.select().from(recipes);
+type FieldKeys = keyof InsertRecipe;
+
+export interface GetRecipeArgs {
+  fields?: FieldKeys[];
+  paginate?: {
+    page: number;
+    pageSize: number;
+  };
+}
+
+const queryFromKeys = (
+  keys: FieldKeys[] | undefined
+): SelectedFields | undefined => {
+  if (!keys?.length) {
+    return undefined;
+  }
+
+  return Object.fromEntries(
+    keys.map((key) => [key, recipes[key]])
+  ) as SelectedFields;
+};
+
+const withPagination = <T extends SQLiteSelect>(
+  qb: T,
+  page: number,
+  pageSize: number
+) => {
+  return qb.limit(pageSize).offset((page - 1) * pageSize);
+};
+
+export const getRecipes = async ({ fields, paginate }: GetRecipeArgs) => {
+  const selectedFields = queryFromKeys(fields);
+  const qb = selectedFields
+    ? db.select(selectedFields).from(recipes).$dynamic()
+    : db.select().from(recipes).$dynamic();
+  if (paginate) {
+    withPagination(qb, paginate.page, paginate.pageSize);
+  }
+  return qb;
+};
+
+export const getRecipeCount = async () => {
+  return db.select({ count: count() }).from(recipes);
 };
 
 export const createRecipe = async (...recipe: InsertRecipe[]) => {
