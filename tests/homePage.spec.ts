a3885c3ccb26972dd28afbd68e0c6967ee4bf459
stilt0n <mattias.leino@arts.cornish.edu>
2024-09-14 00:14:07 -0400

    add site metadata

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Home page is up', { tag: '@smoke' }, async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByText(/sign in/i)).toBeVisible();
-  await expect(page.getByText(/read more/i)).toBeDefined();
+  await expect(
+    page.getByText('Grits Greenbeans and Grandmothers')
+  ).toBeVisible();
 });
