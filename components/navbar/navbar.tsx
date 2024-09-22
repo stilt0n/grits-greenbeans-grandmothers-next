d31863edb637d2d94041713c7d3d49636cb5ed4f
stilt0n <mattias.leino@arts.cornish.edu>
2024-09-21 21:53:16 -0400

    fix user menu bug

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,12 +1,12 @@
 import { Search } from './search.client';
-import { SignInAvatar } from './sign-in-avatar.client';
+import { UserMenu } from './user-menu.client';
 
 const Navbar = () => {
   return (
     <nav className='flex flex-row'>
       <h1 className='text-3xl'>Grits Greenbeans and Grandmothers</h1>
       <Search />
-      <SignInAvatar />
+      <UserMenu />
     </nav>
   );
 };
