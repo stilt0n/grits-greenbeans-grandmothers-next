2d0528b6a600e05b2ff0233dc108b207b4b00c60
stilt0n <mattias.leino@arts.cornish.edu>
2024-09-30 19:45:04 -0400

    fix: return fresh data on form redirect

--- app/create-recipe/page.tsx
+++ app/create-recipe/page.tsx
@@ -2,10 +2,11 @@
 import { useTransition } from 'react';
 import { RecipeForm } from '@/components/recipe-edit-form.client';
 import { formSubmitAction } from '@/app/actions/form-submit';
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 const CreateRecipe = () => {
   const [, startTransition] = useTransition();
+  const router = useRouter();
   return (
     <div>
       <h1 className='text-3xl text-center mt-2'>Add a Recipe</h1>
@@ -14,7 +15,7 @@ const CreateRecipe = () => {
           startTransition(async () => {
             const result = await formSubmitAction(data);
             if (result?.[0]?.recipeId) {
-              redirect(`/recipes/${result[0].recipeId}`);
+              router.push(`/recipes/${result[0].recipeId}`);
             }
           });
         }}
