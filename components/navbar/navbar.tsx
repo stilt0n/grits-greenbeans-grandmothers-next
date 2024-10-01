0580bd0526a518244fb974772db7b53904cb83cf
stilt0n <mattias.leino@arts.cornish.edu>
2024-09-30 21:36:58 -0400

    make title into a link

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import { Search } from './search.client';
 import { UserMenu } from './user-menu.client';
 
 const Navbar = () => {
   return (
     <nav className='flex flex-row'>
-      <h1 className='text-3xl'>Grits Greenbeans and Grandmothers</h1>
+      <h1 className='text-3xl'>
+        <Link href='/'>Grits Greenbeans and Grandmothers</Link>
+      </h1>
       <Search />
       <UserMenu />
     </nav>
