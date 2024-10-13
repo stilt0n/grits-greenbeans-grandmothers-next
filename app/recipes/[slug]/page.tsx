e29b3c720864e3b3ea8162a7ddca68d90604ca83
stilt0n <mattias.leino@arts.cornish.edu>
2024-10-13 02:39:26 -0400

    style recipe page

--- app/recipes/[slug]/page.tsx
+++ app/recipes/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { loadRecipe } from '@/app/actions/load-recipe';
 import { currentUser } from '@clerk/nextjs/server';
 import { hasElevatedPermissions } from '@/lib/auth';
 import { LinkButton } from '@/components/recipe-gallery/link-button.client';
+import { ClockIcon } from '@radix-ui/react-icons';
 
 const Page = async ({ params }: { params: { slug: string } }) => {
   const { slug } = params;
@@ -21,24 +22,34 @@ const Page = async ({ params }: { params: { slug: string } }) => {
 
   return (
     <>
-      <div className='prose prose-zinc'>
-        <h1>{recipe.title}</h1>
+      <div className='prose prose-zinc mx-auto bg-zinc-50 px-4 border-x border-zinc-200 min-h-screen -mt-4 pt-4'>
+        <span className='flex flex-row justify-between'>
+          <h1>{recipe.title}</h1>
+          {hasElevatedPermissions(user) ? (
+            <LinkButton href={`/edit-recipe/${recipeId}`}>Edit</LinkButton>
+          ) : null}
+        </span>
         {recipe.author ? <p>By {recipe.author}</p> : null}
-        {recipe.recipeTime ? <p>Time: {recipe.recipeTime}</p> : null}
+        {recipe.recipeTime ? (
+          <span className='flex flex-row gap-2 items-baseline'>
+            <ClockIcon />
+            {recipe.recipeTime}
+          </span>
+        ) : null}
         {recipe.imageUrl ? (
-          <Image
-            src={recipe.imageUrl}
-            alt={recipe.title}
-            height={300}
-            width={400}
-          />
+          <div className='relative h-96 w-full w-[calc(100%+2rem)] -mx-4'>
+            <Image
+              src={recipe.imageUrl}
+              alt={recipe.title}
+              layout='fill'
+              objectFit='cover'
+              className='object-cover'
+            />
+          </div>
         ) : null}
         {/* HTML here is sanitized on the server-side and should be safe to set */}
         <div dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
       </div>
-      {hasElevatedPermissions(user) ? (
-        <LinkButton href={`/edit-recipe/${recipeId}`}>Edit</LinkButton>
-      ) : null}
     </>
   );
 };
