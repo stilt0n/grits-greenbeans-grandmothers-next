1790256b19121f4c7f1b483bd34c6dcd79eade80
stilt0n <mattias.leino@arts.cornish.edu>
2024-11-12 00:50:46 -0500

    display image preview in recipe form

--- components/recipe-edit-form.client.tsx
+++ components/recipe-edit-form.client.tsx
@@ -9,6 +9,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { FormInput } from '@/components/form/form-input';
 import { Button } from '@/components/ui/button';
 import EditorInput from '@/components/editor';
+import { useEffect, useState } from 'react';
 
 export interface RecipeFormProps {
   onSubmitSuccess: SubmitHandler<RecipeData>;
@@ -25,6 +26,20 @@ const blankRecipeData = {
   instructions: '',
 };
 
+const useImageFileUrl = (fileList: FileList | null) => {
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  useEffect(() => {
+    if (fileList?.length === 1) {
+      const file = fileList[0];
+      const fileUrl = URL.createObjectURL(file);
+      setImageUrl(fileUrl);
+      return () => URL.revokeObjectURL(fileUrl);
+    }
+    setImageUrl(null);
+  }, [fileList]);
+  return imageUrl;
+};
+
 export const RecipeForm = ({
   initialRecipeData = blankRecipeData,
   ...props
@@ -33,13 +48,15 @@ export const RecipeForm = ({
     register,
     handleSubmit,
     setValue,
+    watch,
     formState: { errors: e },
   } = useForm({
     resolver: zodResolver(recipeSchema),
     defaultValues: initialRecipeData,
   });
   const onSubmit = handleSubmit(props.onSubmitSuccess, props.onSubmitError);
-
+  const imageFileList = watch('imageFileList');
+  const imageUrl = useImageFileUrl(imageFileList);
   return (
     <>
       <form
@@ -71,6 +88,12 @@ export const RecipeForm = ({
           accept='image/*'
           {...register('imageFileList')}
         />
+        {
+          // using an img element here because I want to keep the original image size for accurate resizing purposes
+          // alt is blank because the screen reader should already get info from the form input.
+          // eslint-disable-next-line @next/next/no-img-element
+          imageUrl && <img src={imageUrl} alt='' />
+        }
         <EditorInput
           onChange={(editorContent) => setValue('instructions', editorContent)}
           menuAriaLabel='instructions editor menu'
