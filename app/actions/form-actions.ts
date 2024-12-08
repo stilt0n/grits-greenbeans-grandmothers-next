1fec91ef38dcb6dbb93f851d9d755e2fadf20aff
stilt0n <mattias.leino@arts.cornish.edu>
2024-12-08 13:07:44 -0500

    create local upload script

--- app/actions/form-actions.ts
+++ app/actions/form-actions.ts
@@ -73,6 +73,7 @@ export const recipeCreateAction = async (
         console.log('no image buffer!');
         return;
       }
+      await writeImageBufferToFile(imageBuffer);
       recipeData.imageUrl = await uploadImageToS3(image, cropCoordinates);
     }
     // The html from TipTap is already sanitized but it is still
