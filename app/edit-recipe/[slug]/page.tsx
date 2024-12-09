860adc7008e1c330804796320256794f76741d2e
stilt0n <mattias.leino@arts.cornish.edu>
2024-12-09 18:00:39 -0500

    do not cache recipe edit page

--- app/edit-recipe/[slug]/page.tsx
+++ app/edit-recipe/[slug]/page.tsx
@@ -1,9 +1,11 @@
 import { notFound } from 'next/navigation';
 import { loadRecipe } from '@/app/actions/load-recipe';
 import { EditRecipe } from './edit-recipe.client';
+import { unstable_noStore as noStore } from 'next/cache';
 import { Suspense } from 'react';
 
 const Page = async ({ params }: { params: { slug: string } }) => {
+  noStore();
   const { slug } = params;
   const recipeId = parseInt(slug);
   if (Number.isNaN(recipeId)) {
