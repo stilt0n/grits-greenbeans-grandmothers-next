e4899acd2c2fb2d2c10e2a64265de31c614813cb
stilt0n <mattias.leino@arts.cornish.edu>
2024-12-08 20:36:23 -0500

    update test with new title

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test('Home page is up', { tag: '@smoke' }, async ({ page }) => {
   await page.goto('/');
   await expect(
-    page.getByText('Grits Greenbeans and Grandmothers')
+    page.getByText('Grits, Greenbeans and Grandmothers')
   ).toBeVisible();
 });
