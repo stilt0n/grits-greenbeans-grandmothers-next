503dd587be56739565842082de994db3dcba5880
stilt0n <mattias.leino@arts.cornish.edu>
2024-12-08 20:26:40 -0500

    add comma to site title

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className='flex flex-row px-4 py-4 drop-shadow-xl border-b border-zinc-200 items-baseline h-18'>
       <h1 className='text-lg md:text-3xl text-left flex self-start'>
-        <Link href='/'>Grits Greenbeans and Grandmothers</Link>
+        <Link href='/'>Grits, Greenbeans and Grandmothers</Link>
       </h1>
       <div className='flex flex-row justify-end ml-auto gap-6'>
         <Search className='hidden lg:flex' />
