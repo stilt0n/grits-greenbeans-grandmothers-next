afaf99fec97c54be01b1f95adcca7098efca5a12
stilt0n <mattias.leino@arts.cornish.edu>
2024-12-10 02:33:10 -0500

    create schema for tags

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { sql } from 'drizzle-orm';
+import { relations, sql } from 'drizzle-orm';
 import { integer, text, sqliteTable } from 'drizzle-orm/sqlite-core';
 
 export const recipes = sqliteTable('recipes', {
@@ -17,5 +17,39 @@ export const recipes = sqliteTable('recipes', {
   imageUrl: text('image_url'),
 });
 
+export const tags = sqliteTable('tags', {
+  id: integer('id').primaryKey(),
+  name: text('name').notNull().unique(),
+});
+
+export const recipesToTags = sqliteTable('recipe_tag', {
+  id: integer('id').primaryKey(),
+  recipeId: integer('recipe_id').references(() => recipes.id),
+  tagId: integer('tag_id').references(() => tags.id),
+});
+
+export const recipeRelations = relations(recipes, ({ many }) => ({
+  recipesToTags: many(recipesToTags),
+}));
+
+export const tagRelations = relations(tags, ({ many }) => ({
+  recipesToTags: many(recipesToTags),
+}));
+
+export const recipeToTagRelations = relations(recipesToTags, ({ one }) => ({
+  recipe: one(recipes, {
+    fields: [recipesToTags.recipeId],
+    references: [recipes.id],
+  }),
+  tag: one(tags, {
+    fields: [recipesToTags.tagId],
+    references: [tags.id],
+  }),
+}));
+
 export type InsertRecipe = typeof recipes.$inferInsert;
 export type SelectRecipe = typeof recipes.$inferSelect;
+export type InsertTag = typeof tags.$inferInsert;
+export type SelectTag = typeof tags.$inferSelect;
+export type InsertRecipesToTags = typeof recipesToTags.$inferInsert;
+export type SelectRecipesToTags = typeof recipesToTags.$inferSelect;
