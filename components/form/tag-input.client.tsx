a80b9bcf86543f72725484ce0d0353c44d1b23e2
stilt0n <mattias.leino@arts.cornish.edu>
2024-12-12 22:15:34 -0500

    create tag input component

--- components/form/tag-input.client.tsx
+++ components/form/tag-input.client.tsx
@@ -1,11 +1,14 @@
 'use client';
 
-import { useState, KeyboardEvent } from 'react';
+import { useState } from 'react';
 import { FormInput } from './form-input';
 import { Button } from '@/components/ui/button';
+import { Tag } from './tag.client';
+import { PlusIcon } from '@radix-ui/react-icons';
 
 export interface TagInputProps {
   label: string;
+  className?: string;
 }
 
 export const TagInput = (props: TagInputProps) => {
@@ -19,22 +22,38 @@ export const TagInput = (props: TagInputProps) => {
   };
 
   return (
-    <div>
-      {tags.map((t) => (
-        <p key={t}>{t}</p>
-      ))}
-      <FormInput
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        label={props.label}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            e.preventDefault();
-            onTagCreate();
-          }
-        }}
-      />
-      <Button onClick={onTagCreate}>Create Tag</Button>
+    <div className={props.className}>
+      <div className='flex flex-row max-w-full overflow-x-auto gap-x-2'>
+        {tags.map((text, index) => (
+          <Tag
+            key={text}
+            text={text}
+            onClose={() => {
+              setTags((current) => current.toSpliced(index, 1));
+            }}
+          />
+        ))}
+      </div>
+      <div className='flex flex-row gap-x-2 items-center'>
+        <FormInput
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          label={props.label}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              onTagCreate();
+            }
+          }}
+        />
+        <Button
+          className='self-end'
+          aria-label='create tag'
+          onClick={onTagCreate}
+        >
+          <PlusIcon />
+        </Button>
+      </div>
     </div>
   );
 };
