4abfc924e759969a964a7d6099c5794bb110659f
stilt0n <mattias.leino@arts.cornish.edu>
2024-12-17 18:28:20 -0500

    refactor: rewrite schemas

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,3 +1,4 @@
+/** @deprecated */
 export {
   getRecipes,
   getRecipesWithTags,
---
+++ lib/translation/schema.ts
@@ -0,0 +1,88 @@
+import { z } from 'zod';
+
+const isValidCropCoordinate = (str: string) => {
+  try {
+    const { success } = cropCoordinateSchema.safeParse(JSON.parse(str));
+    return success;
+  } catch {
+    return false;
+  }
+};
+
+const isValidJsonArray = (str: string) => {
+  try {
+    const parsed = JSON.parse(str);
+    return Array.isArray(parsed);
+  } catch {
+    return false;
+  }
+};
+
+const Integer = z.number().transform(Math.floor);
+
+export const cropCoordinateSchema = z.object({
+  x: Integer,
+  y: Integer,
+  width: Integer,
+  height: Integer,
+});
+
+export const recipeFormSchema = z.object({
+  title: z.string().min(1, 'Title cannot be blank'),
+  description: z
+    .string()
+    .min(1, 'Description cannot be blank')
+    .max(
+      255,
+      'Recipe descriptions are intended to be small and should be less than 255 characters'
+    ),
+  instructions: z
+    .string()
+    .min(1, 'Instructions cannot be blank and cannot use the default template'),
+  author: z.string().nullable().default(null),
+  recipeTime: z.string().nullable().default(null),
+  imageFileList: z
+    .unknown()
+    .transform((value) => value as FileList | null)
+    .refine(
+      (fl) => fl === null || fl.length === 1,
+      'Upload a maximum of one image'
+    ),
+  cropCoordinates: z
+    .string()
+    .nullable()
+    .refine((str) => str === null || isValidCropCoordinate(str)),
+  tags: z
+    .string()
+    .nullable()
+    .refine((str) => str === null || isValidJsonArray(str)),
+});
+
+export const baseRecipeSchema = z.object({
+  title: z.string().min(1, 'Title cannot be blank'),
+  description: z
+    .string()
+    .min(1, 'Description cannot be blank')
+    .max(
+      255,
+      'Recipe descriptions are intended to be small and should be less than 255 characters'
+    ),
+  instructions: z
+    .string()
+    .min(1, 'Instructions cannot be blank and cannot use the default template'),
+  author: z.string().optional().default('unknown author'),
+  recipeTime: z.string().optional(),
+});
+
+export const tagsSchema = z.array(z.string()).optional();
+
+export const recipePageSchema = baseRecipeSchema.extend({
+  imageUrl: z.string().optional(),
+  tags: tagsSchema,
+});
+
+export type RecipeFormData = z.infer<typeof recipeFormSchema>;
+export type BaseRecipe = z.infer<typeof baseRecipeSchema>;
+export type TagsArray = z.infer<typeof tagsSchema>;
+export type RecipePageData = z.infer<typeof recipePageSchema>;
+export type CropCoordinates = z.infer<typeof cropCoordinateSchema>;
