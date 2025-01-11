f88504a341a1deb3a58253a8f29c3b1111dc1713
stilt0n <mattias.leino@arts.cornish.edu>
2025-01-11 18:49:13 -0500

    feat: support search by category

--- lib/actions/load-page-count.ts
+++ lib/actions/load-page-count.ts
@@ -1,19 +1,22 @@
 'use server';
 
+import { SearchCategory } from '@/lib/translation/schema';
 import { getRecipeCount } from '@/lib/repository/recipe-store/read';
 
 export interface LoadPageCountArgs {
   pageSize: number;
   filter?: string;
+  category?: SearchCategory;
   debug?: boolean;
 }
 
 export const loadPageCountAction = async ({
   pageSize,
   filter,
+  category,
   debug,
 }: LoadPageCountArgs) => {
-  const [{ count }] = await getRecipeCount(filter);
+  const [{ count }] = await getRecipeCount(filter, category);
   if (debug) {
     console.log(`Got page count ${count}`);
   }
